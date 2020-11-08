09441d1f2a2390067ca181270ff5b72f2498f36d
Abbas SAHIN <sahi.sah201678@gmail.com>
2020-11-08 20:42:23 +0100

    second commit from local fature branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -17,6 +17,7 @@ public class FirstClass {
 
 		System.out.println("Here is another conflict from local master branch");
 		System.out.println("Here are some changes from local feature branch");
+		System.out.println("here is the conflict on local feature");
 		
 
 
