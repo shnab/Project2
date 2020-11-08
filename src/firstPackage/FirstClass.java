8631638cebe915d95838d49bf2618b075d1661ed
shnab <47714325+shnab@users.noreply.github.com>
2020-11-08 17:54:25 +0100

    second commit from the remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
 		System.out.println("second commit from local master");
 		System.out.println("third commit from the local master");
 		System.out.println("first commit from the remote master branch");
+		System.out.println("second commit from the remote master branch");
 
 	}
 
