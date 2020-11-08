26dca16cbb4eb64a90179aef431e5040ec23dfdf
Abbas SAHIN <sahi.sah201678@gmail.com>
2020-11-08 17:13:54 +0100

    thirs commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("first commit from local master branch");
+		System.out.println("second commit from local master");
+		System.out.println("third commit from the local master");
 
 	}
 
